46e825cbfa79be0dfbc9a8d2b76d07e62d50df52
Emix999 <emilianhernes2@gmail.com>
2025-02-12 20:28:41 +0100

    Update js.js

--- js.js
+++ js.js
@@ -75,7 +75,7 @@ let lista_imion = ["test0","test1","test2"];
 
 
 function losowanie_nazwy(){
-    let ilosc_imion_w_liscie;
+
     let losowa_liczba=Math.floor(Math.random()*lista_imion.length);
     let wylosowane_imie= lista_imion[losowa_liczba];
     document.getElementById('pole_nazwy').value = wylosowane_imie;
