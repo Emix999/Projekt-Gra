c8eac7262378c86437e7853a7ef9f0336f9d0184
Emix999 <emilianhernes2@gmail.com>
2025-02-12 20:22:51 +0100

    losowanie nazw

--- js.js
+++ js.js
@@ -1,1 +1,54 @@
 console.log("test")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function losowanie_nazwy(){
+    let ilosc_imion_w_liscie;
+    let losowa_liczba=Math.floor(Math.random()*lista_imion.length);
+    let wylosowane_imie= lista_imion[losowa_liczba];
+    document.getElementById('pole_nazwy').value = wylosowane_imie;
+}
