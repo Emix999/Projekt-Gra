f7c00e2d6fa70ca4cbc5afb39485fed2f0207429
Jan01234567890 <janek.appinventor@gmail.com>
2025-02-13 20:49:42 +0100

    js menu graczy łatka

--- js.js
+++ js.js
@@ -1,61 +1,11 @@
 function rozwin_menu_gracza(gracz_menu, gracz_rozwin, gracz_zwin){
-    gracz_menu.style.visibility = 'visible';
-    gracz_rozwin.style.visibility = 'hidden';
-    gracz_zwin.style.visibility = 'visible';
+    document.getElementById(gracz_menu).style.visibility = 'visible';
+    document.getElementById(gracz_rozwin).style.visibility = 'hidden';
+    document.getElementById(gracz_zwin).style.visibility = 'visible';
 }
 
 function zwin_menu_gracza(gracz_menu, gracz_rozwin, gracz_zwin){
-    gracz_menu.style.visibility = 'hidden';
-    gracz_rozwin.style.visibility = 'visible';
-    gracz_zwin.style.visibility = 'hidden';
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let lista_imion = ["test0","test1","test2"];
-
-
-
-
-function losowanie_nazwy(){
-
-    let losowa_liczba=Math.floor(Math.random()*lista_imion.length);
-    let wylosowane_imie= lista_imion[losowa_liczba];
-    document.getElementById('pole_nazwy').value = wylosowane_imie;
+    document.getElementById(gracz_menu).style.visibility = 'hidden';
+    document.getElementById(gracz_rozwin).style.visibility = 'visible';
+    document.getElementById(gracz_zwin).style.visibility = 'hidden';
 }
